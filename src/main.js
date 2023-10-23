c4344a428d6c7a984c96ce5f558e5aaace0b8bfd
Dmitry Pimenov <dmitry.pimenov@webflow.com>
2023-10-23 12:48:27 -0700

    updating babel dependencies

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 const App = () => {
   return (
     <div>
-      <h1>Welcome to My React App!</h1>
+      <h1>Welcome to My React App</h1>
       <p>This is a basic React application.</p>
     </div>
   );
